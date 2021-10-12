9d9a6369db1e897b59d4a6e6ba5d0025dbe438a9
Валерія Попова <valeriia.popova@nure.ua>
2021-10-12 18:53:12 +0300

    1

--- ClientPS/Program.cs
+++ ClientPS/Program.cs
@@ -8,35 +8,16 @@ namespace ClientPS
     internal static class Program
     {
         public static void Main(string[] args)
-        {
-            //ChannelFactory<ITaskService> factory = new ChannelFactory<ITaskService>(new WebHttpBinding(),
-            //    new EndpointAddress("http://localhost:44301/TaskService"));
-            //factory.Endpoint.Behaviors.Add(new WebHttpBehavior());
-
-            //ITaskService client = factory.CreateChannel();
-
-
-            //var resultFirst = client.ChangeOfStatusFirstMethod();
-            //Console.WriteLine(resultFirst);
-
-            //var resultSecond = client.ChangeOfStatusSecondMethod();
-            //Console.WriteLine(resultSecond);
-        
-
-
+        {      
+            
             ChannelFactory<IRejectionService> factory = new ChannelFactory<IRejectionService>(new WebHttpBinding(),
                new EndpointAddress("http://localhost:52000/RejectionService"));
             factory.Endpoint.Behaviors.Add(new WebHttpBehavior());
 
             IRejectionService client = factory.CreateChannel();
 
-
             var resultFirst = client.TimerForRejection("100000");
             Console.WriteLine(resultFirst);
-
-
-
-
 
             Console.ReadKey();
 
